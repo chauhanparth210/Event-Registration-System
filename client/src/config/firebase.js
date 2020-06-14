871db2d0accd8ab4f4f16440dd8fa5210143d0ec
chauhanparth210 <00chauhanparth@gmail.com>
2020-06-14 11:45:40 +0530

    final submission

---
+++ client/src/config/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+//  initialize firebase object from firebase console
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
---
+++ client/src/config/index.js
@@ -0,0 +1,4 @@
+export default {
+  URL: "", /// Backend API URL
+  SECRET: "", /// Provide Same SECRET which is on Backend .env file
+};
